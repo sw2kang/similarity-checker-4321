966af8058a6606220267b18af495e02efe7ef9f9
seungwoo <lirtha@naver.com>
2024-06-13 16:39:07 +0900

    [feature] add validation related features

--- similarity_checker.py
+++ similarity_checker.py
@@ -1,2 +1,13 @@
 class SimilarityChecker:
-    pass
+    def check_validity(self, word1, word2):
+        for c in word1:
+            if not ord('A') <= ord(c) <= ord('Z'):
+                raise TypeError()
+        for c in word2:
+            if not ord('A') <= ord(c) <= ord('Z'):
+                raise TypeError()
+        if len(word1) == 0 or len(word2) == 0:
+            raise TypeError()
+
+    def check_count(self, word1, word2):
+        self.check_validity(word1, word2)
--- test_similarity_checker.py
+++ test_similarity_checker.py
@@ -1,5 +1,36 @@
 from unittest import TestCase
 
+from similarity_checker import SimilarityChecker
+
 
 class TestSimilarityChecker(TestCase):
-    pass
+    def setUp(self):
+        super().setUp()
+        self.checker = SimilarityChecker()
+
+    def count_validity_fail_check(self, args):
+        try:
+            self.checker.check_count(*args)
+            self.fail()
+        except TypeError:
+            pass
+
+    def count_validity_pass_check(self, args):
+        try:
+            self.checker.check_count(*args)
+        except:
+            self.fail()
+
+    def test_count_failing_validity(self):
+        self.count_validity_fail_check(('ab', 'cd'))
+        self.count_validity_fail_check(('ab',))
+        self.count_validity_fail_check(('ab', 'cd', 'ef'))
+        self.count_validity_fail_check(('ab', None))
+        self.count_validity_fail_check(('ABC',))
+        self.count_validity_fail_check(('ABC', 'BBC', 'CDE'))
+        self.count_validity_fail_check(('', ''))
+        self.count_validity_fail_check(('ABC', ''))
+
+    def test_count_passing_validity(self):
+        self.count_validity_pass_check(('AB', 'CDEF'))
+        self.count_validity_pass_check(('ABCD', 'A'))
