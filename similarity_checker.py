efd5088358e010ffeb65f49e4ff2ffb24f2b8686
seungwoo <lirtha@naver.com>
2024-06-13 16:02:00 +0900

    [feature] add validity check

--- similarity_checker.py
+++ similarity_checker.py
@@ -1,3 +1,10 @@
 class SimilarityChecker:
-    def check_length(self, param, param1):
-        raise TypeError()
+    def check_length(self, word1, word2):
+        for c in word1:
+            if not ord('A') <= ord(c) <= ord('Z'):
+                raise TypeError()
+
+        for c in word2:
+            if not ord('A') <= ord(c) <= ord('Z'):
+                raise TypeError()
+
--- test_similarity_checker.py
+++ test_similarity_checker.py
@@ -4,10 +4,38 @@ from similarity_checker import SimilarityChecker
 
 
 class TestSimilarityChecker(TestCase):
-    def test_validity(self):
+
+    def setUp(self):
+        super().setUp()
+        self.checker = SimilarityChecker()
+
+    def test_failing_validity(self):
         try:
-            checker = SimilarityChecker()
-            checker.check_length('ab', 'cd')
+            self.checker.check_length('ab', 'cd')
             self.fail()
         except TypeError:
             pass
+
+        try:
+            self.checker.check_length('ab', 'cd', 'ef')
+            self.fail()
+        except TypeError:
+            pass
+
+        try:
+            self.checker.check_length('ab')
+            self.fail()
+        except TypeError:
+            pass
+
+        try:
+            self.checker.check_length('ab', None)
+            self.fail()
+        except TypeError:
+            pass
+
+    def test_passing_validity(self):
+        try:
+            self.checker.check_length('AB', 'CDEF')
+        except:
+            self.fail()
