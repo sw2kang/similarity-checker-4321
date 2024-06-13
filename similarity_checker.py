d7118c31d8d667940a0fb098c7b3700edfd56bfa
seungwoo <lirtha@naver.com>
2024-06-13 16:15:21 +0900

    [refactoring] code refactor

--- similarity_checker.py
+++ similarity_checker.py
@@ -1,18 +1,18 @@
 class SimilarityChecker:
-    def check_length(self, word1, word2):
+    def check_validity(self, word1, word2):
         for c in word1:
             if not ord('A') <= ord(c) <= ord('Z'):
                 raise TypeError()
-
         for c in word2:
             if not ord('A') <= ord(c) <= ord('Z'):
                 raise TypeError()
-
         if len(word1) == 0 or len(word2) == 0:
             raise TypeError()
+
+    def check_length(self, word1, word2):
+        self.check_validity(word1, word2)
 
         if len(word1) == len(word2):
             return 60
 
         return 0
-
